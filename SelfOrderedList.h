a55797b3fd9ac0da8e5cc729f120f5574482f28c
Anthony N <antnat96@gmail.com>
2020-06-28 19:20:42 -0500

    implementation file added

---
+++ SelfOrderedList.h
@@ -0,0 +1,60 @@
+/* 
+ * File:   SelfOrderedList.h
+ * Author: Anthony Natale
+ *
+ * Created on June 28th, 2020
+ */
+
+#ifndef SELFORDEREDLIST_H
+#define	SELFORDEREDLIST_H
+
+#include "SelfOrderedListADT.h"
+
+template <typename E> class SelfOrderedList : public SelfOrderedListADT<E> {
+
+public:
+    //Default constructor/destructor
+    SelfOrderedListADT(){}
+    ~SelfOrderedListADT() {}
+    
+    //Look for 'it'.  If found return true and execute the self-ordering
+    //heuristic used for ordering the list: count, move-to-front, or transpose.
+    //Increments the compare instance variable every time it compares 'it' to
+    //other members of the list. Returns true if 'it' is found.
+	bool find(const E& it) {
+	
+	}
+    
+    //Called by find if 'it' is not in the list. Adds the new 'it' to the list
+    //Can also be called independently when initially loading the list with
+    //unique values and when you want to load the list in the order 'it' is 
+    //read without your re-order method being called (or the number of compares
+    //being incremented.
+	void add(const E& it) {  //Add new 'it' to the list
+
+	}
+
+	int getCompares() const {  //Returns the number of accumulated compares
+
+	}
+
+	int size() const {       //Returns the size of the list
+
+	}
+    
+    //Print the list
+    //In order print of the list.  printlist() prints the entire list
+    //whereas printlist(n) prints n nodes.
+	void printlist() {
+
+	}
+
+	void printlist(int n) {
+
+	}
+    
+};
+
+
+#endif	/* SELFORDEREDLIST_H */
+
